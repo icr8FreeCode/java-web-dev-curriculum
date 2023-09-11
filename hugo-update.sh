c5f74e632688c14d7556e52bc5b1fe4384e986d8
Rob Thomas <rob@launchcode.org>
2023-09-11 16:31:53 -0500

    updated and removed submodules

---
+++ hugo-update.sh
@@ -0,0 +1,9 @@
+git rm -r --cached themes layouts static archetypes; 
+# git rm -R themes layouts static archetypes
+
+# git submodule add https://github.com/LaunchCode-Hugo-Submodules/hugo-theme-relearn themes/hugo-theme-relearn; 
+#  echo 'theme = "hugo-theme-relearn"' >> config.toml; 
+  
+# git submodule add https://github.com/LaunchCode-Hugo-Submodules/layouts; 
+# git submodule add https://github.com/LaunchCode-Hugo-Submodules/static; 
+# git submodule add https://github.com/LaunchCode-Hugo-Submodules/archetypes; 
